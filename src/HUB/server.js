bc8e329296548564b1ee49413e7eeb3d74406a97
Jackson Gurney <jacko7557@gmail.com>
2022-10-06 16:28:19 -0700

    make a server.js

---
+++ src/HUB/server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const io = require('socket.io');
+const PORT = process.env.PORT || 3002;
+
+const server = io(PORT);
+const haiku = server.of('/haiku');
+const handleGame = require('./gameRunner');
+
+haiku.on('connection', (socket) => {
+  console.log('New Client connected!!');
+
+
+  socket.on('join', (payload) => {
+    socket.join(payload.clientId);
+    console.log('Registered in room :', payload.clientId);
+    clientList.push(payload.clientName);
+    if(clientList.length === 3){
+      //start the game
+      handleGame(clientList);
+    }
+  });
+});
+
+
+// socket.emit('join', { clientId: 'Haiku Game 1', clientName: 'Jackson' })
+
